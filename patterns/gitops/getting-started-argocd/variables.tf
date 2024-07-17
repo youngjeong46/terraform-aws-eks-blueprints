a51d83d575aa08684e4748ab4900cbfc75a998db
Young <youngjeong46@gmail.com>
2024-07-17 09:08:03 -0500

    changed to forked address

--- patterns/gitops/getting-started-argocd/variables.tf
+++ patterns/gitops/getting-started-argocd/variables.tf
@@ -25,7 +25,7 @@ variable "addons" {
 variable "gitops_addons_org" {
   description = "Git repository org/user contains for addons"
   type        = string
-  default     = "https://github.com/aws-samples"
+  default     = "https://github.com/youngjeong46"
 }
 variable "gitops_addons_repo" {
   description = "Git repository contains for addons"
@@ -52,7 +52,7 @@ variable "gitops_addons_path" {
 variable "gitops_workload_org" {
   description = "Git repository org/user contains for workload"
   type        = string
-  default     = "https://github.com/aws-ia"
+  default     = "https://github.com/youngjeong46"
 }
 variable "gitops_workload_repo" {
   description = "Git repository contains for workload"
